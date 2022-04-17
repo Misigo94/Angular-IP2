2fb2e7de4258507045e9f8b2ae7301343ae835d2
Misigomartin <Misigomartin@gmail.com>
2022-04-18 00:22:59 +0300

    add attributes to the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(
+        public name: string,
+        public followers: number,
+        public following: number,
+        public Bio: string,
+        public created: string,
+        public url:string,
+        public repos:number
+
+    ) {}
 }
