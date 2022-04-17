0bc1a99c201b49a3641e8477a91f7c3d96822706
Misigomartin <Misigomartin@gmail.com>
2022-04-18 00:35:32 +0300

    add attributes to the class repository

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,15 @@
 export class Repository {
+    public display:boolean
+    constructor(
+        public description: string,
+        public name: string,
+        public forks: number,
+        public watching: number,
+        public languages: string,
+        public url: string,
+        public stars:number
+    ) {
+        this.display = true;
+}
+
 }
