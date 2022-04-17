36b688bb83b184ed019907b7cc93d6eb70f3e808
Misigomartin <Misigomartin@gmail.com>
2022-04-18 00:26:02 +0300

    add thr httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+ import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FormComponent } from './form/form.component';
 import { BodyComponent } from './body/body.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FormComponent,
-    BodyComponent
+    BodyComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
