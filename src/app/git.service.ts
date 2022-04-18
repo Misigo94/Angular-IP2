c071d0591b7f2ce44d31d3bc214413a8b4819ae8
Misigomartin <Misigomartin@gmail.com>
2022-04-18 23:26:49 +0300

    ad promise to the file

--- src/app/git.service.ts
+++ src/app/git.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { lastValueFrom } from 'rxjs';
+import { User } from './user';
+import { Repository } from './repository';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
-
-  constructor() { }
+ 
+  constructor(private http: HttpClient) { }
+  async getUser(userName:string) {
+    const promise = this.http.get(`${environment.githubUrl}/users/${userName}`);
+    return await lastValueFrom(promise)
+      .then((result) => result)
+      .catch((error) => console.log(error));
+  }
 }
+  
+  
+
+
