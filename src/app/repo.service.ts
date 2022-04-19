d05576e0e4271b41a1994f1c3611ec8b7ff9853d
Misigomartin <Misigomartin@gmail.com>
2022-04-19 09:32:11 +0300

    add changes to the file

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -2,14 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient,} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { lastValueFrom } from 'rxjs';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
+  repository:Repository; 
+	repoData = [];
+  newUserData: any = [];
+ 
 
-  constructor(private http: HttpClient) { }
-  async getRepo(reponame: any) {
-    const promise = this.http.get(`${environment.githubUrl}/repo/${reponame}`)
+  constructor(private http: HttpClient) {
+   this.repository = new Repository("","","",new Date(),"","","",new Date())
   }
-}
+  getUserData(username: string) {
+
+    
+    
+    // async getRepo(reponame: any) {
+    //   const promise = this.http.get(`${environment.githubUrl}/repo/${reponame}`)
+    //   return await lastValueFrom(promise)
+    //   .then((result) => result)
+    //     .catch((error) => console.log(error));
+    // }
+  }
