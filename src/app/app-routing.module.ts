e5db400dbd0d8a314dcfe585d84f230d98f85236
Misigomartin <Misigomartin@gmail.com>
2022-04-19 11:10:31 +0300

    import http module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { AppComponent } from './app.component';
+import {LandingComponent} from './landing/landing.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'app', component: AppComponent },
+  {path:'',component:BodyComponent},
+  { path: 'body', component: BodyComponent },
+  {path:'landing',component:LandingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
